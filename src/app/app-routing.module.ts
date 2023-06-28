d4b805935e4a2aceb9666c32de1c99813528cbe2
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2023-06-28 14:52:27 -0300

    Some routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  //{ path: 'professionals', component:  },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
